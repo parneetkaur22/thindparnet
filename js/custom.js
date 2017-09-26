b9f6ba3a676d2d0033a6f86c90d2340d4aeca751
parneetkaur22 <31783681+parneetkaur22@users.noreply.github.com>
2017-09-26 17:12:53 -0400

    demo repo

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+// Custom JavaScript
+// STEP 15: Call up the carousel using the id, and use the .carousel() method create the options object
+
+
+// STEP 16: Change the interval to 5 seconds (set in milliseconds)
+// STEP 17: Try an event handler to zoom up the .carousel-caption after the slide completes sliding - by adding a class when the slide has finished animating, and removing it when it begins
+
+  $('#carousel-cars').on('slide.bs.carousel', function(){
+      // remove class that zooms caption
+      $('.carousel-caption').removeClass('zoomit');
+  })
+  $('#carousel-cars').on('slid-bs carousel' , function(){
+      // add class that zooms 
+  })
